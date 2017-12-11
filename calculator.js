236932c7e590b67daed2a63957f29ecc6f275b4e
Joseph Kaestner <joseph.kaestner@warriorlife.net>
2017-12-11 14:21:07 -0800

    finished version 1

--- calculator.js
+++ calculator.js
@@ -1,1 +1,17 @@
 // JavaScript Document
+function d (val){
+	document.getElementById("d").value = val;
+}
+function v(val){
+	document.getElementById("d").value +=val;
+}
+function e() {
+try{
+	d(eval(document.getElementById("d").value))
+}
+catch(err) {
+	d("Error");
+}
+}
+	
+	
